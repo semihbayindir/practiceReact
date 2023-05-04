93bc01cbedd7211c3712c8f9359366bc548be7a5
semih bayındır <semihbayindir@192.168.1.79>
2023-05-04 22:48:33 +0300

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button} from 'react-native';
+import React, {useState} from 'react';
 
 export default function App() {
+  const[name,setName] = useState('Semih');
+  const[person,setPerson] =useState({name:'Furkan', age: 27});
+  
+  const clickHandler = () => {
+    setName('Koray');
+    setPerson({name:'Koray', age: 23});
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Text>Hello! {name}</Text>
+        <Text>{person.name} {person.age} years old.</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title='Click Me!' onPress={clickHandler}></Button>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +31,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  buttonContainer: {
+    marginTop:20,
   },
 });
